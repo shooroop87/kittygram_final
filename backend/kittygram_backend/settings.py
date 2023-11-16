359342bab96ce88023f8d10f6abba72391307889
Andrey <shooroop87@mail.ru>
2023-11-16 13:23:12 +0300

    git kitty review 9

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,9 +8,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-
 DEBUG = os.getenv('DEBUG_VALUE') == 'True'
-
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
 
 INSTALLED_APPS = [
