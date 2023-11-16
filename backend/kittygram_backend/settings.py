6c5553bfb01a995ebfdf6b6549724caf668c8bff
Andrey <shooroop87@mail.ru>
2023-11-16 14:05:10 +0300

    git kitty review 13

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = str(os.getenv("DJANGO_SECRET_KEY"))
 DEBUG = os.getenv('DEBUG_VALUE') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = str(os.getenv("ALLOWED_HOSTS"))
 
 INSTALLED_APPS = [
     'django.contrib.admin',
