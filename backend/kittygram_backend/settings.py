f14574fddf50be2af5756c9eb90bc28eee488e0e
Andrey <shooroop87@mail.ru>
2023-11-16 13:56:34 +0300

    git kitty review 11

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ load_dotenv(find_dotenv())
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = str(os.getenv("DJANGO_SECRET_KEY"))
 DEBUG = os.getenv('DEBUG_VALUE') == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
 
