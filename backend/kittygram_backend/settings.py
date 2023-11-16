8dffd1ee51af872ea14c89f62c2b4443511cd993
Andrey <shooroop87@mail.ru>
2023-11-16 12:59:35 +0300

    git kitty review 5

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG_VALUE') == 'True'
 
